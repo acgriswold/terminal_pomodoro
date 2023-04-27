7b5205b257422b204e4145c589aaf93ebfaef7eb
Adam Griswold <adam.griswold@nucor.com>
2023-04-27 12:50:32 +0000

    IO events are now handled in a separate thread

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-use std::cell::RefCell;
 use std::io::stdout;
-use std::rc::Rc;
+use std::sync::Arc;
 use std::time::Duration;
 
 use app::{App, AppReturn};
 use eyre::Result;
 use inputs::events::Events;
 use inputs::InputEvent;
+use io::IoEvent;
 use tui::backend::CrosstermBackend;
 use tui::Terminal;
 
@@ -14,8 +14,10 @@ use crate::app::ui;
 
 pub mod app;
 pub mod inputs;
+pub mod io;
 
-pub fn start_ui(app: Rc<RefCell<App>>) -> Result<()> {
+pub async fn start_ui(app: &Arc<tokio::sync::Mutex<App>>) -> Result<()> {
+    // Configure Crossterm backend for tui
     let stdout = stdout();
     crossterm::terminal::enable_raw_mode()?;
     let backend = CrosstermBackend::new(stdout);
@@ -25,26 +27,35 @@ pub fn start_ui(app: Rc<RefCell<App>>) -> Result<()> {
 
     // User event handler
     let tick_rate = Duration::from_millis(200);
-    let events = Events::new(tick_rate);
+    let mut events = Events::new(tick_rate);
+
+    // Trigger state change from Init to Initialized
+    {
+        let mut app = app.lock().await;
+        // Here we assume the the first load is a long task
+        app.dispatch(IoEvent::Initialize).await;
+    }
 
     loop {
-        let mut app = app.borrow_mut();
+        let mut app = app.lock().await;
 
         // Render
         terminal.draw(|rect| ui::draw(rect, &app))?;
 
         // Handle inputs
-        let result = match events.next()? {
-            InputEvent::Input(key) => app.do_action(key),
-            InputEvent::Tick => app.update_on_tick(),
+        let result = match events.next().await {
+            InputEvent::Input(key) => app.do_action(key).await,
+            InputEvent::Tick => app.update_on_tick().await,
         };
-
+        
         // Check if we should exit
         if result == AppReturn::Exit {
+            events.close();
             break;
         }
     }
 
+    // Restore the terminal and close application
     terminal.clear()?;
     terminal.show_cursor()?;
     crossterm::terminal::disable_raw_mode()?;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::sync::Arc;
 
 use eyre::Result;
 use terminal_pomodoro::app::App;
+use terminal_pomodoro::io::handler::IoAsyncHandler;
+use terminal_pomodoro::io::IoEvent;
 use terminal_pomodoro::start_ui;
 
-fn main() -> Result<()> {
-    let app = Rc::new(RefCell::new(App::new()));
-    start_ui(app)?;
+#[tokio::main]
+async fn main() -> Result<()> {
+    let (sync_io_tx, mut sync_io_rx) = tokio::sync::mpsc::channel::<IoEvent>(100);
+
+    // Setup app instances to share between threads
+    let app = Arc::new(tokio::sync::Mutex::new(App::new(sync_io_tx.clone())));
+    let app_ui = Arc::clone(&app);
+
+    // Handle IO in a specifc thread
+    tokio::spawn(async move {
+        let mut handler = IoAsyncHandler::new(app);
+        while let Some(io_event) = sync_io_rx.recv().await {
+            handler.handle_io_event(io_event).await;
+        }
+    });
+
+    start_ui(&app_ui).await?;
+
     Ok(())
 }
